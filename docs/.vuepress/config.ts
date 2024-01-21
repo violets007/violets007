f5a333923809eef2d7b9299a3642d3633b453213
zixuan2020 <1186019009@qq.com>
2024-01-21 20:06:27 +0800

    update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -28,7 +28,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
   themeConfig: {
     // 导航配置
     nav: [
-      { text: '首页', link: '/' },
+      // { text: '首页', link: '/' },
       // {
       //   text: '前端',
       //   link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
@@ -76,27 +76,27 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       //     { text: 'CSS', link: '/pages/0a83b083bdf257cb/' },
       //   ],
       // },
-      {
-        text: '技术',
-        link: '/technology/',
-        items: [
-          // { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
-          // { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
-          // { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
-          // { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
-        ],
-      },
-      {
-        text: '更多',
-        link: '/more/',
-        items: [
-          // { text: '学习', link: '/pages/f2a556/' },
-          // { text: '面试', link: '/pages/aea6571b7a8bae86/' },
-          // { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
-          // { text: '实用技巧', link: '/pages/baaa02/' },
-        ],
-      },
-      { text: '关于', link: '/about/' },
+      // {
+      //   text: '技术',
+      //   link: '/technology/',
+      //   items: [
+      //     // { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
+      //     // { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
+      //     // { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
+      //     // { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+      //   ],
+      // },
+      // {
+      //   text: '更多',
+      //   link: '/more/',
+      //   items: [
+      //     // { text: '学习', link: '/pages/f2a556/' },
+      //     // { text: '面试', link: '/pages/aea6571b7a8bae86/' },
+      //     // { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
+      //     // { text: '实用技巧', link: '/pages/baaa02/' },
+      //   ],
+      // },
+      // { text: '关于', link: '/about/' },
       // {
       //   text: '收藏',
       //   link: '/pages/beb6c0bd8a66cea6/',
